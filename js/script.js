7a5636682d598df838b73f3178f342a84d01531a
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-03-16 19:57:16 +0100

    EjercicioNoResuelto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,48 @@
 import peliculas from './peliculas.js'
 
+const elements = document.getElementsByClassName('genero');
+console.log('elemento', elements)
+ 
+// hacer filtro por generos 28 53 12 
+//bucle {imagen, titulo} for (let i=0; i <= peliculas.lenght, i++)
+
+// let accion =peliculas.filter(x=>x.genre_ids == 28);
+// let thriller = peliculas.filter (i => i.genre_ids == 53);
+// let aventura = peliculas.filter(x=>x.genre_ids == 12);
+
+// for ( let i = 0; i<= peliculas.length; i++){
+//     console.log(action);
+    
+
+// capturar el elemento del HTML y que se repita
+
+
+
+
+
+// const imprimirPeliculas = () => {
+    
+//     let peliculasgenre = {};
+//     for (let pelicula of peliculas){
+
+//         if (!peliculasgenre[pelicula.genre_ids]){
+//             peliculasgenre[pelicula.genre_ids] = [];
+//         }
+//         peliculasgenre[pelicula.genre_ids].push(pelicula.title);
+//     };
+//     const pelis = document.getElementById('genero');
+//    // pelis.innerHTML = '';
+
+
+//     }
+// }
+
+// // Llamamos a la función para mostrar las películas agrupadas
+// imprimirPeliculas(peliculas);
+    
+
+
+//ME ES IMPOSIBLE COMPRENDER EL EJERCICIO. NO SE QUE PASOS TENGO QUE SEGUIR PARA LLEGAR A LA CONCLUSION DEL EJERCICIO.
+
+  
+  
